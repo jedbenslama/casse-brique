da0e28ba1adc527b2c90e151fd542030042bcb8c
jedd <jed@jed.jed>
2025-12-08 13:39:21 +0100

    Decor + Fin

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include "conioremade.c"
+#include <string.h>
 
 char lireCommandeNonBloquante(int nombreinput[3]) {
     int kb_is_hit=0;
@@ -24,22 +25,22 @@ char lireCommandeNonBloquante(int nombreinput[3]) {
 }
 
 void afficherGrille(int grille[50][30]){
-    printf("--------------------------------\n");
+    printf("--------------------------------------------------------------\n");
     for (int i = 0; i < 50; i++){
         printf("|");
         for (int j = 0; j < 30; j++){
             switch (grille[i][j]){
             case 0:
-                printf(" ");
+                printf("⬜️");
                 break;
             case 1:
-                printf("#");
+                printf("🟫");
                 break;
             case 2:
-                printf("O");
+                printf("🉐");
                 break;
             case 3:
-                printf("=");
+                printf("⬛️");
                 break;
             default:
                 printf("Erreur 1\n");
@@ -50,7 +51,7 @@ void afficherGrille(int grille[50][30]){
         printf("|");
         printf("\n");
     }
-    printf("--------------------------------\n");
+    printf("--------------------------------------------------------------\n");
 }
 
 void initGrille(int grille[50][30]){
@@ -70,11 +71,9 @@ void initGrille(int grille[50][30]){
     grille[49][15] = 3;
     grille[49][16] = 3;
     grille[48][15] = 2;
-
-    grille[5][0] = 0; // pour test
     
 }
-int deplacement_balle(int grille[50][30], int statut_balle[1]){
+int deplacement_balle(int grille[50][30], int statut_balle[1], int donnees_joueur[10]){
     int jballe = -1;
     int iballe;
     int status = 0;
@@ -107,7 +106,8 @@ int deplacement_balle(int grille[50][30], int statut_balle[1]){
             grille[iballe][jballe]=0;
             grille[iballe-1][jballe-1]=2;
         }
-        if(grille[iballe-1][jballe-1]==1){
+        if(grille[iballe-1][jballe-1]==1){ // cassage brique
+            donnees_joueur[0]+=10;
             grille[iballe-1][jballe-1]=0;
             statut_balle[0]=2;
         }
@@ -129,7 +129,8 @@ int deplacement_balle(int grille[50][30], int statut_balle[1]){
             grille[iballe][jballe]=0;
             grille[iballe-1][jballe+1]=2;
         }
-        if(grille[iballe-1][jballe+1]==1){
+        if(grille[iballe-1][jballe+1]==1){ // cassage brique
+            donnees_joueur[0]+=10;
             grille[iballe-1][jballe+1]=0;
             statut_balle[0]=3;
         }
@@ -144,7 +145,8 @@ int deplacement_balle(int grille[50][30], int statut_balle[1]){
             grille[iballe+1][jballe-1]=2;
         }else if(grille[iballe+1][jballe-1]==3){
             statut_balle[0] = 0;
-        }else if(grille[iballe+1][jballe-1]==1){
+        }else if(grille[iballe+1][jballe-1]==1){ // cassage brique
+            donnees_joueur[0]+=10;
             grille[iballe+1][jballe-1]=0;
             statut_balle[0] = 0;
             if(jballe>=1){
@@ -165,7 +167,8 @@ int deplacement_balle(int grille[50][30], int statut_balle[1]){
             grille[iballe+1][jballe+1]=2;
         }else if(grille[iballe+1][jballe+1]==3){
             statut_balle[0] = 1;
-        }else if(grille[iballe+1][jballe+1]==1){
+        }else if(grille[iballe+1][jballe+1]==1){ // cassage brique
+            donnees_joueur[0]+=10;
             grille[iballe+1][jballe+1]=0;
             statut_balle[0] = 1;
             if(jballe<=28){
@@ -194,7 +197,7 @@ int deplacement_plateforme(int grille[50][30]){
     }
     int nombreinput[2] = {0};
     char input = lireCommandeNonBloquante(nombreinput);
-    if (input=='q' || input=='a'){
+    if (input=='A' || input=='Q' || input=='q' || input=='a'){
         for (int i = 0; i < nombreinput[0]; i++){
             if (horizontale>=1){
                 grille[49][horizontale+2]=0;
@@ -202,7 +205,7 @@ int deplacement_plateforme(int grille[50][30]){
                 horizontale-=1;
             }
         }
-    }else if (input=='d'){
+    }else if (input=='d' || input=='D'){
         for (int i = 0; i < nombreinput[0]; i++){
             if (horizontale<=26){
                 grille[49][horizontale+3]=3;
@@ -215,20 +218,46 @@ int deplacement_plateforme(int grille[50][30]){
     }
     return 0;
 }
+
+void saveScore(int donnees_joueur[10]){
+    clearScreen();
+    char username[100]; 
+    printf("Bien joué, ton score final était de %d !\nSaisis ton nom pour enregistrer ton score.\n> ", donnees_joueur[0]);
+    fgets(username, 20, stdin);
+    username[strcspn(username, "\n")] = 0; // référence sur https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
+    printf("Le nom %s a été enregistré avec un score de %d", username, donnees_joueur[0]);
+    FILE * fichier = fopen("scores.txt", "a+");
+    fprintf(fichier, "%s %d\n", username, donnees_joueur[0]);
+    fclose(fichier);
+}
+
 int main(){
     int grille[50][30];
     initGrille(grille);
     int statut_balle[1] = {1}; // 0 = haut gauche; 1 = haut droite; 2 = bas gauche; 3 = bas droite
     // 0 = vide; 1 = brique; 2 = balle; 3 = plateforme
+    int donnees_joueur[10];
+    donnees_joueur[0] = 0;
+    donnees_joueur[1] = 1;
+    int temps_debut = time(NULL);
     while(1){
+        if(time(NULL) > temps_debut + 15){ // la difficulté augmente toutes les 15 secondes
+            donnees_joueur[1] += 1;
+            temps_debut = time(NULL);
+        }
         clearScreen();
         afficherGrille(grille);
-        Sleep(100); // 800 par defaut
+        printf("Score: %d\nNiveau actuel: %d\n", donnees_joueur[0], donnees_joueur[1]);
+        Sleep(100 / donnees_joueur[1]); // la vitesse augmente selon le niveau de difficulté
         if(deplacement_plateforme(grille) == 1){
+            saveScore(donnees_joueur);
             break;
         }
-        if(deplacement_balle(grille, statut_balle) == 1){
-            printf("Perdu !\n");
+        if(deplacement_balle(grille, statut_balle, donnees_joueur) == 1){
+            // on reclear et re affiche la grille pour eviter la pollution visuelle
+            clearScreen();
+            afficherGrille(grille);
+            printf("Score: %d\nNiveau actuel: %d\nPerdu !\n", donnees_joueur[0], donnees_joueur[1]);
             break;
         };
     }
