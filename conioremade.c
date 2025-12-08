7b6a01aef427d2e8c110dbcefadf1c05201afbe5
jedd <jed@jed.jed>
2025-12-08 12:54:48 +0100

    clearscreen test

--- conioremade.c
+++ conioremade.c
@@ -3,8 +3,16 @@
     #include <conio.h>
     #include <windows.h>
     void clearScreen(){
-        system("cls");
+        HANDLE hOut;
+        COORD Position;
+
+        hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+        Position.X = 0;
+        Position.Y = 0;
+        SetConsoleCursorPosition(hOut, Position);
     }
+
 #else
     #include <termios.h>
     #include <fcntl.h>
